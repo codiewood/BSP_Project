176132e24bbd5e001018191a6519840bfbcf0fae
Codie <codie.wood@mansfield.ox.ac.uk>
2020-12-15 14:55:10 +0000

    Update main.py
    Introduced class structure for cell monolayer

--- ci_course/main.py
+++ ci_course/main.py
@@ -1,4 +1,3 @@
-import numpy as np
 from numpy import random
 from math import sqrt
 
@@ -20,9 +19,59 @@ def rand_pert(n, mag=0.05, time_step=0.005):
 
     Returns
     -------
-    numpy.ndarray
+    numpy.array
         The random perturbation force, an n x 1 array.
     """
     x = random.normal(0, 1, n)
-    force = sqrt(2*mag/time_step)*x
+    force = sqrt(2 * mag / time_step) * x
     return force
+
+
+def uniform_coords(lim):
+    """
+    Generates uniformly distributed random coordinates in the 2D square, (0,lim)^2.
+
+    Parameters
+    ----------
+    lim : int, float
+        Upper bound of coordinate value in both the x and y directions.
+
+    Returns
+    -------
+    tuple
+        2D coordinates.
+    """
+    x = round(random.uniform(lim), 3)
+    y = round(random.uniform(lim), 3)
+    return x, y
+
+
+class Monolayer:
+    """Monolayer of cells in 2D space"""
+    def __init__(self, n, p=0.5, size=20):
+        """
+        Initialises properties of the monolayer.
+
+        Parameters
+        ----------
+        n : int
+            Number of cells present in the monolayer
+
+        p : int, float
+            Proportion of cells which are of Type 0, taking values in [0,1]. Default is 0.5.
+
+        size : int, float
+            Size of the monolayer (maximum x and y coordinate for the cell centres). Default is 20.
+        """
+        self.num_cells = n
+        self.size = size
+        self.type_0 = round(n * p)
+        self.type_1 = n - self.type_0
+        self.initial_positions = self.generate_cells()
+
+    def generate_cells(self):
+        cell_positions = []
+        for i in list(range(self.num_cells)):
+            x = uniform_coords(self.size)
+            cell_positions.append(x)
+        return cell_positions
